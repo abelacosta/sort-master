45f0e1df419c560dfb5e916cec96b176bae201c9
Abel Acosta <abelacosta1596@gmail.com>
2020-06-16 22:33:59 -0700

    Changed formatting

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import NavBar from './components/navbar';
-import Graph from './components/graph';
-import ToolPanel from './components/toolpanel';
+import React from "react";
+import NavBar from "./components/navbar";
+import Graph from "./components/graph";
+import ToolPanel from "./components/toolpanel";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <div className="d-flex justify-content-center">
-        <Graph/>
+        <Graph />
         <ToolPanel />
       </div>
     </div>
--- src/components/graph.jsx
+++ src/components/graph.jsx
@@ -1,14 +1,10 @@
 import React, { Component } from "react";
 
-class Graph extends Component{
-    state = {}
-    render(){
-        return (
-            <canvas className="bg-primary">
-                This is a canvas.
-            </canvas>
-        )
-    }
+class Graph extends Component {
+  state = {};
+  render() {
+    return <canvas className="bg-primary">This is a canvas.</canvas>;
+  }
 }
 
 export default Graph;
