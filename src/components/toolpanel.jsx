98f8d933219a47144fcd03c391834c6cffe709c5
Abel Acosta <abelacosta1596@gmail.com>
2020-06-18 15:41:37 -0700

    Changed button layout

--- src/components/toolpanel.jsx
+++ src/components/toolpanel.jsx
@@ -3,7 +3,16 @@ import React, { Component } from "react";
 class ToolPanel extends Component {
   render() {
     return (
-      <div className="p-4">
+      <div className="w-25 p-5">
+        <button
+          onClick={() => {
+            this.props.onRandomize();
+          }}
+          type="button"
+          className="btn btn-warning btn-lg w-100 m-1"
+        >
+          Randomize
+        </button>
         <h6>Number of objects:</h6>
         <input
           type="range"
@@ -12,58 +21,69 @@ class ToolPanel extends Component {
           }}
           min="4"
           max="100"
-          className="w-75"
+          className="w-100"
         ></input>
         <h6>Sorting methods:</h6>
-        <button
-          onClick={() => {
-            this.props.onMethod("bubble");
-          }}
-          type="button"
-          className="btn btn-info btn-lg m-1"
-        >
-          Bubble Sort
-        </button>
-        <button
-          onClick={() => {
-            this.props.onMethod("heap");
-          }}
-          type="button"
-          className="btn btn-info btn-lg m-1"
-        >
-          Heap Sort
-        </button>
-        <button
-          onClick={() => {
-            this.props.onMethod("quick");
-          }}
-          type="button"
-          className="btn btn-info btn-lg m-1"
-        >
-          Quick Sort
-        </button>
-        <button
-          onClick={() => {
-            this.props.onMethod("merge");
-          }}
-          type="button"
-          className="btn btn-info btn-lg m-1"
-        >
-          Merge Sort
-        </button>
-        <h6>Actions:</h6>
-        <button
-          onClick={() => {
-            this.props.onRandomize();
-          }}
-          type="button"
-          className="btn btn-warning btn-lg m-1"
-        >
-          Randomize
-        </button>
-        <button type="button" className="btn btn-success btn-lg m-1">
-          Sort
-        </button>
+        <div className="row">
+          <button
+            onClick={() => {
+              this.props.onMethod("bubble");
+            }}
+            type="button"
+            className="btn btn-info col m-1"
+          >
+            Bubble Sort
+          </button>
+          <button
+            onClick={() => {
+              this.props.onMethod("selection");
+            }}
+            type="button"
+            className="btn btn-info col m-1"
+          >
+            Selection Sort
+          </button>
+        </div>
+        <div className="row">
+          <button
+            onClick={() => {
+              this.props.onMethod("insertion");
+            }}
+            type="button"
+            className="btn btn-info col m-1"
+          >
+            Insertion Sort
+          </button>
+          <button
+            onClick={() => {
+              this.props.onMethod("heap");
+            }}
+            type="button"
+            className="btn btn-info col m-1"
+          >
+            Heap Sort
+          </button>
+        </div>
+        <div className="row">
+          <button
+            onClick={() => {
+              this.props.onMethod("quick");
+            }}
+            type="button"
+            className="btn btn-info col m-1"
+          >
+            Quick Sort
+          </button>
+          <button
+            onClick={() => {
+              this.props.onMethod("merge");
+            }}
+            type="button"
+            className="btn btn-info col m-1"
+          >
+            Merge Sort
+          </button>
+        </div>
       </div>
     );
   }
